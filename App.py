2370469a60701092312f0cd4b63a8f28d939dbe0
Jhoan Mena <104227792+JhoanSho@users.noreply.github.com>
2023-08-24 11:16:23 -0500

    Add files via upload

---
+++ App.py
@@ -0,0 +1,15 @@
+from flaskr import create_app
+from .modelos import db, Cancion
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+#Prueba
+with app.app_context():
+    c = Cancion(titulo='Prueba', minutos=2, segundos=25, interprete='Joes')
+    db.session.add(c)
+    db.session.commit()
+    print(Cancion.query.all())
